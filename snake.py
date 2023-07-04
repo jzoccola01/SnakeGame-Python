f1be2ccf2ab7bc1da5187d620e39dac8a7e6ad25
zachnino <zani7674@colorado.edu>
2023-07-04 13:01:43 -0600

    made screen

--- snake.py
+++ snake.py
@@ -1,1 +1,14 @@
-print("Hello Jonny!")
+import pygame
+import sys
+
+pygame.init()
+# screen display
+screen = pygame.display.set_mode((400, 500))
+
+while True:
+    # draw elements
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
