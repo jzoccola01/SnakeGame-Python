5342cfa9e310c4498cf183f7224eab6463579f73
zachnino <zani7674@colorado.edu>
2023-07-05 20:11:01 -0600

    CURRENTS

--- snake.py
+++ snake.py
@@ -1,9 +1,49 @@
 import pygame
 import sys
+import random
+from pygame.math import Vector2
+
+
+class FOOD:
+    def __init__(self):
+        self.x = random.randint(0, cell_number-1)
+        self.y = random.randint(0, cell_number-1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_food(self):
+        food_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, (200, 0, 0), food_rect)
+
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1, 0)
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = block.x * cell_size
+            y_pos = block.y * cell_size
+            block_rec = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            pygame.draw.rect(screen, (0, 200, 0), block_rec)
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy[:]
+
 
 pygame.init()
-# screen display
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+
+food = FOOD()
+snake = SNAKE()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 while True:
     # draw elements
@@ -11,4 +51,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0, -1)
+    screen.fill('pink')
+    food.draw_food()
+    snake.draw_snake()
     pygame.display.update()
+    clock.tick(60)
