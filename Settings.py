724584aca3525b699515be2ea5dcad8cb5615cc5
Jonny Zoccola <81589052+jzoccola01@users.noreply.github.com>
2023-07-05 23:01:22 -0700

    Added score, adding to snake, and eating food
    Signed-off-by: jzoccola01 <jzoccola@calpoly.edu>

--- Settings.py
+++ Settings.py
@@ -1,12 +1,12 @@
-import pygame
 import sys
+
+import pygame
 
 # Settings:
 cell_number = 40
 cell_size = 20
 width, height = cell_number * cell_size, cell_number * cell_size
 screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
 
 
 def get_cell_number() -> int:
@@ -25,15 +25,20 @@ def set_cell_number(num) -> int:
     return num
 
 
+def set_screen_size(width, height):
+    return pygame.display.set_mode((width, height))
+
+
 def get_screen():
     return screen
 
 
 def get_clock():
-    return clock
+    return pygame.time.Clock()
 
 
+# TODO: Make this function display a game over screen instead of just quitting the game
 def game_over():
+    # Pause the game and display game over screen (eventually)
     pygame.quit()
     sys.exit()
-
